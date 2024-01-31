6eb4dc25eda80f8b1508b9fc4bcb3f220d9c5045
Pablo Mikhno <p.yu.mihno@inbox.ru>
2024-02-01 00:31:18 +0300

    new: Download File function

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"AvailableVpn/internal/domain"
+	"fmt"
+)
 
+func main() {
+	err := domain.DownloadFile("https://ipspeed.info/ovpn/219.100.37.83_udp_1195.ovpn", "./ovpn/test.ovpn")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
--- internal/domain/AvailableVpn.go
+++ internal/domain/AvailableVpn.go
@@ -1,1 +1,31 @@
 package domain
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	_ "github.com/gocolly/colly/v2"
+)
+
+func DownloadFile(urlFrom, fileTo string) error {
+	_, err := os.Stat(fileTo)
+	var file *os.File
+	if os.IsNotExist(err) {
+		file, _ = os.OpenFile(fileTo, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	} else {
+		return errors.New("File already exists")
+	}
+	resp, err := http.Get(urlFrom)
+	if resp.StatusCode != 200 {
+		return errors.New("Failed to download file")
+	}
+	w, _ := io.Copy(file, resp.Body)
+	fmt.Println(w)
+	defer file.Close()
+	defer resp.Body.Close()
+
+	return nil
+}
