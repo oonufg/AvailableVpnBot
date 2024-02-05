927c0d89f9600eabadd7d5a8ecb0a0896958607e
Mikhno Pavel <69582699+oonufg@users.noreply.github.com>
2024-02-05 15:42:17 +0300

    upd

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	ctx := context.Background()
 	repo := domain.CreateOvpnRepository()
-	b := bot.CreateBot("6865414091:AAFXg2YL-f_W5zJFLKbvzVTRT6gzIYstCz4", repo)
+	b := bot.CreateBot("Token", repo)
 	b.Start(ctx)
 }
